59a300c9d060b51d41b0b11faac5244fcb03a84b
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-09-14 17:32:08 +0300

    Update citire_lista_float.py

--- lectie_practica/citire_lista_float.py
+++ lectie_practica/citire_lista_float.py
@@ -1,16 +1,26 @@
 from math import fabs
-
-
 lista1 = []
+
+def isfloat(a):
+    try:
+        float(a)
+        return True
+    except:
+        return False
+
+
 def creare_lista():
     lista=[]
     lungime =int(input("dati lungimea listei: "))
     for i in range(lungime):
         elem =input("Elementul "+str(i+1)+" : ")
-        while(elem.isnumeric()== False):
+        if elem == "exit":
+            return 0
+        while(isfloat(elem) == False):
             elem = input("Elementul "+str(i+1)+" : ")
 
         lista.append(float(elem))
     return lista
+    
 lista1 = creare_lista()
 print(lista1)
